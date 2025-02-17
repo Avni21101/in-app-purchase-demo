37dc0430489898b1f8e14477ac1991739bd5a743
Avni <avniprajapati21101@gmail.com>
2025-02-17 13:17:49 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/filter_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter.boilerplate.demo.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: Center(child: Text('In app purchase', style: TextStyle(fontSize: 30))),
+    );
+  }
+}
